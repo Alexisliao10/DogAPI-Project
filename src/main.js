008536c4cd3187a5c4ff710b66cd7e50a46fde0f
Alexis Liao <Alexisliao10@hotmail.com>
2023-12-05 21:06:38 -0500

    RemoveDog function no loger make multiple petition to the api, bugging it

--- src/main.js
+++ src/main.js
@@ -55,76 +55,90 @@ async function getDog() {
         addFavourites(data[index].id);
       }
     });
-
     getFavourites();
   } catch (error) {
     throw new Error(error.message);
   }
 }
+
+const favouriteDogs = [];
+
+const section = document.querySelector("#favourites");
+const title = document.createElement("h2");
+title.textContent = "Your favorite doggos";
+
 async function getFavourites() {
   try {
     const data = await fetchData(API_URL_FAVOURITE);
-    console.log("🚀 ~ file: main.js:68 ~ getFavourites ~ data:", data);
-
-    const section = document.querySelector("#favourites");
-    const title = document.createElement("h2");
-    title.textContent = "Your favorite doggos";
-
-    renders = data.map((item, index) => {
-      const figure = document.createElement("figure");
-      const img = document.createElement("img");
-      const btn = document.createElement("button");
-
-      btn.textContent = "Unlike";
-      btn.classList = "like-button";
-      img.width = 300;
-      img.src = item.image.url;
-
-      figure.append(img, btn);
-
-      btn.dataset.index = index;
-      return figure.outerHTML;
-    });
-
-    section.innerHTML = [title.outerHTML, ...renders].join("");
-
-    section.addEventListener("click", (event) => {
-      if (event.target.classList.contains("like-button")) {
-        const index = parseInt(event.target.dataset.index, 10);
-        removeFavorite(data[index].id);
-      }
-    });
+    favouriteDogs.push(...data);
+    renderFavourite();
   } catch (error) {
     throw new Error(error);
   }
 }
 
-async function addFavourites(id) {
-  const response = await fetch(API_URL_FAVOURITE, {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify({
-      image_id: id,
-    }),
-  });
+function renderFavourite() {
+  renders = favouriteDogs.map((item, index) => {
+    const figure = document.createElement("figure");
+    const img = document.createElement("img");
+    const btn = document.createElement("button");
 
-  if (response.status === 200) {
-    console.log("Saved to favourites");
-    getFavourites();
-  } else {
-    console.log("Failed to add to favourites");
+    btn.textContent = "Unlike";
+    btn.classList = "like-button";
+    img.width = 300;
+    img.src = item.image.url;
+
+    figure.append(img, btn);
+
+    btn.dataset.index = index;
+    return figure.outerHTML;
+  });
+  section.innerHTML = [title.outerHTML, ...renders].join("");
+}
+
+section.addEventListener("click", (event) => {
+  if (event.target.classList.contains("like-button")) {
+    const index = parseInt(event.target.dataset.index, 10);
+    removeFavorite(favouriteDogs[index].id);
+  }
+});
+
+async function addFavourites(id) {
+  try {
+    const response = await fetch(API_URL_FAVOURITE, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify({
+        image_id: id,
+      }),
+    });
+
+    if (response.status === 200) {
+      console.log("Saved to favourites");
+      favouriteDogs.length = 0;
+      getFavourites();
+    } else {
+      console.log("Failed to add to favourites");
+    }
+  } catch (error) {
+    throw new Error(error.message);
   }
 }
 
 async function removeFavorite(id) {
-  const response = await fetch(API_URL_FAVOURITE + `/${id}`, {
-    method: "DELETE",
-    headers: headers,
-  });
-  if (response.status === 200) {
-    console.log("Deleted from favorites");
-    getFavourites();
-  } else {
-    spanError.innerText = "There's an error: " + response.status;
+  try {
+    const response = await fetch(API_URL_FAVOURITE + `/${id}`, {
+      method: "DELETE",
+      headers: headers,
+    });
+    if (response.status === 200) {
+      favouriteDogs.length = 0;
+      await new Promise((resolve) => setTimeout(resolve(getFavourites()), 500));
+      console.log("Deleted from favorites");
+    } else {
+      spanError.innerText = "There's an error: " + response.status;
+    }
+  } catch (error) {
+    throw new Error(error);
   }
 }
